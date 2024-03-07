d588110cf0b64cc2771312594591bb9bebb92625
BerdanKoc <Berdan.koc@epsi.fr>
2024-03-07 10:33:05 +0100

    passage en vert du test DeuxPlusZero mais le test UnPlusZero ne marche plus donc faudra faire un theory

--- StringCalculatorProjet/StringCalculator.cs
+++ StringCalculatorProjet/StringCalculator.cs
@@ -4,7 +4,7 @@
     {
         public static int Parse()
         {
-            var input = "0,1";
+            var input = "2,0";
             var parts = input.Split(',');
             return int.Parse(parts.First()) + int.Parse(parts.Last());
         }
