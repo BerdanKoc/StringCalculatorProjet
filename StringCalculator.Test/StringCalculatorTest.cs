d5df23933a8991ec757499c70be5afe0f2cf966e
BerdanKoc <Berdan.koc@epsi.fr>
2024-03-07 10:27:42 +0100

    Ajout du test pour le cas 2+0 (pas valide)

--- StringCalculator.Test/StringCalculatorTest.cs
+++ StringCalculator.Test/StringCalculatorTest.cs
@@ -13,5 +13,16 @@ namespace StringCalculator.Test
 
             Assert.Equal(parts.Sum(), result);
         }
+        [Fact]
+        public void DeuxPlusZero()
+        {
+            var parts = new int[] { 2, 0 };
+
+            var input = string.Join(",", parts);
+
+            var result = StringCalculator.Parse();
+
+            Assert.Equal(parts.Sum(), result);
+        }
     }
 }   
