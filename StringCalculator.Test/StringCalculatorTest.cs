238fe3e32dda48a9e5155d58e5284e2d3459c11c
BerdanKoc <Berdan.koc@epsi.fr>
2024-03-07 10:37:02 +0100

    Ajout du test theory (Pas valide) pour généraliser le test pour toutes les combinaisons possibles à 2 chiffres

--- StringCalculator.Test/StringCalculatorTest.cs
+++ StringCalculator.Test/StringCalculatorTest.cs
@@ -2,6 +2,26 @@ namespace StringCalculator.Test
 {
     public class StringCalculatorTest
     {
+        public static IEnumerable<object[]> CasAPlusB()
+        {
+            yield return [0, 0];
+            yield return [1, 0];
+            yield return [0, 1];
+            yield return [2, 0];
+            yield return [0, 2];
+        }
+
+        [Theory]
+        [MemberData(nameof(CasAPlusB))]
+        public void APlusB(int[] parts)
+        {
+            var input = string.Join(",", parts);
+
+            var result = StringCalculator.Parse();
+
+            Assert.Equal(parts.Sum(), result);
+        }
+
         [Fact]
         public void UnPlusZero()
         {
